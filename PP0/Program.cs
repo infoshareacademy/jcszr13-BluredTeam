c85d4e4d22d80e1b7bb3677d9d953a26f79ff82c
Stelmer87 <adam.stelmaszuk@gmail.com>
2024-04-09 00:40:09 +0200

    commit on branch AdamStelmaszuk_LoginRegisterLogic. Created static class LoginUtilities.cs with static method  ProvideYourCredential(). Need to handle Registration flow.

--- PP0/Program.cs
+++ PP0/Program.cs
@@ -1,9 +1,31 @@
-﻿namespace PP0
+﻿using PP0.StaticClasses;
+
+namespace PP0
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            
+            bool exitMainMenu = false;
+            do
+            { 
+                LoginUtilities.MainLoginRegisterWindow();
+                System.ConsoleKey key = Console.ReadKey().Key;
+
+                if (key == ConsoleKey.L)
+                {
+                    LoginUtilities.LoginCredentialsWindow();
+                }
+                else if (key == ConsoleKey.R) {
+                    Console.WriteLine("Show Registration Window");
+                }
+                else if (key == ConsoleKey.X)
+                {
+                    exitMainMenu = true;
+                }
+
+            } while (!exitMainMenu);
             
         }
     }
---
+++ PP0/StaticClasses/LoginUtilities.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PP0.StaticClasses
+{
+    internal static class LoginUtilities
+    {
+        /// <summary>
+        /// This method pops up Main Menu. You can Login or Create New Account
+        /// </summary>
+        internal static void MainLoginRegisterWindow()
+        {
+            Console.Clear();
+            Console.WriteLine($"Press 'L' for LOG INTO\nPress 'R' to REGISTER\nPress 'X' to abort");
+        } 
+
+        internal static void LoginCredentialsWindow()
+        {
+            bool nextStep;
+            if (nextStep = ProvideYourCredential("Login"))
+                if (nextStep = ProvideYourCredential("Password"))
+                    if (nextStep = ProvideYourCredential("Role"))
+                    {
+                        Console.WriteLine("LOGGED SUCCESFULL!");
+                        Console.ReadKey();
+                    }
+                    else
+                        MainLoginRegisterWindow();
+
+                else
+                    MainLoginRegisterWindow();
+            else
+                MainLoginRegisterWindow();
+            
+
+        }
+
+        internal static bool ProvideYourCredential(string credentailType)
+        {
+            Console.Clear();
+            Console.WriteLine($"To go back to Main Login Window press 'Esc' Key.");
+            Console.WriteLine($"Please provide your {credentailType}:");
+
+            string input = "";
+
+            while(true)
+            {
+                ConsoleKeyInfo key = Console.ReadKey(true);
+
+                if (key.Key == ConsoleKey.Escape)
+                {
+                    return false;
+                }
+                else if (key.Key == ConsoleKey.Enter)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine($"{credentailType} provided: {input}");
+                    Console.ReadKey();
+                    return true;
+                }
+                else {
+                    input += key.KeyChar;
+                }
+            }
+
+        }
+
+
+
+    }
+}
