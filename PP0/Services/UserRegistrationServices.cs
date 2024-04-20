f92b564382c05fbc54f3050271b80352f9efc97e
KrzysztofStanio <krzysztof.stanio@nfz.gov.pl>
2024-04-20 21:33:42 +0200

    Add  service class UserRegistrationServices by metods: SetAllUsers, GetAllUsers, GetUserByLogi, CreateNewAccount, ValidateNewAccount

---
+++ PP0/Services/UserRegistrationServices.cs
@@ -0,0 +1,55 @@
+﻿using PP0.Models;
+using System.Collections.Generic;
+
+namespace PP0.Services
+{
+    internal class UserRegistrationServices
+    {
+        private List<User> _users = new List<User>();
+
+        public void SetAllUsers(List<User> users) { _users = users; }
+
+        public List<User> GetAllUsers() { return _users; }
+
+
+        public User? GetUserByLogin(string login)
+        {
+
+            foreach (var user in _users)
+            {
+                if (user.Login == login)
+                {
+                    return user;
+                }
+            }
+
+            return null;
+        }
+
+        public bool CreateNewAccount(User user)
+        {
+            if (_users.Count == 0 || GetUserByLogin(user.Login) is null)
+            {
+                _users.Add(user);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public string ValidateNewAccount(User user)
+        {
+            if (user.Login == null || user.Password == null || user.Roles == null)
+            {
+                return $"incorrect data";
+            }
+            else
+            {
+                return $"correct data";
+            }
+        }
+
+    }
+}
