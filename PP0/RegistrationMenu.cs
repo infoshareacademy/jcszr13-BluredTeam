e23bd509d94f11e07a6483b5fd1a4ed05d3db3f5
KrzysztofStanio <krzysztof.stanio@nfz.gov.pl>
2024-04-20 21:40:13 +0200

    menu do rejestracji nowego użytkownika -testowe dodatkowo enum RoleType

---
+++ PP0/RegistrationMenu.cs
@@ -0,0 +1,54 @@
+﻿using PP0.Enums;
+using PP0.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PP0
+{
+    internal class RegistrationMenu
+    {
+        public static bool MainRegistrationMenu()
+        {
+            Console.Clear();
+            Console.WriteLine($"Enter user details:");
+            Console.Write("Enter login: ");
+            string login = Console.ReadLine();
+            Console.Write("Enter password: ");
+            string password = Console.ReadLine();
+            Console.WriteLine("Choose role (user, doctor, admin): ");
+            List<Role> roles = UserRoleList();
+
+            User newUser = new User(login, password, roles);
+            return true;
+        }
+
+
+
+        public static List<Role> UserRoleList()
+        {
+            var _roles = new List<Role>();
+            //var _role = new Role();
+            //System.ConsoleKey key = Console.ReadKey().Key;
+
+            foreach (RoleType item in RoleType.GetValues(typeof(RoleType)))
+            {
+                Console.Write($"add role {RoleType.GetName(item)} (press Y): ");
+                System.ConsoleKey key = Console.ReadKey().Key;
+                if (key == ConsoleKey.Y)
+                {
+                    //_role.Id = (int)item;
+                    //_role.Name = (RoleType.GetName(item));
+                    //_roles.Add(_role);
+                    _roles.Add(new Role { Id = (int)item, Name = item.ToString() });
+                }
+                Console.WriteLine();
+            }
+
+            return _roles;
+        }
+
+    }
+}
