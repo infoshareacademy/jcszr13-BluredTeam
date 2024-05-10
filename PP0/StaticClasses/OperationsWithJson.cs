f83e3c19f715a683b4f502520cfb4c9384015f9c
Stelmer87 <adam.stelmaszuk@gmail.com>
2024-05-10 23:33:04 +0200

    commit on branch AdamStelmaszuk_Logowanie. Created class OperationsWithJson.cs and method DeserilizeJson. In Program.cs added a line of code that invokes method DesirilizeJson from static class and returns list of users in DB.

---
+++ PP0/StaticClasses/OperationsWithJson.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace PP0.StaticClasses
+{
+    internal static class OperationsWithJson
+    {
+        private static readonly JsonSerializerOptions _options = new() { WriteIndented = true };
+
+        internal static List<T> DeserilizeJson<T>(string fileName) {
+            Stream streamJson = new FileStream(fileName,FileMode.Open,FileAccess.Read);
+            return JsonSerializer.Deserialize<List<T>>(streamJson, _options);
+        }
+
+    }
+}
