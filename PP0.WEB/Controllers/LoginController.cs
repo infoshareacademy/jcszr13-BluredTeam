d867c9505637da8a20a028ee858d4a8fb066e101
Stelmer87 <adam.stelmaszuk@gmail.com>
2024-07-16 00:00:12 +0200

    adding LoginController.cs and Login View Index.cshtml

---
+++ PP0.WEB/Controllers/LoginController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PP0.WEB.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
