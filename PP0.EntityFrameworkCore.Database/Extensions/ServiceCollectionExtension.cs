562d1848a740a833e12374682481d20ed5dd9661
KrzysztofStanio <krzysztof.stanio@nfz.gov.pl>
2024-07-18 13:09:45 +0200

    Create the _LoginPartial.cshtml file https://learn.microsoft.com/en-us/entra/identity-platform/tutorial-web-app-dotnet-sign-in-users?tabs=visual-studio

--- PP0.EntityFrameworkCore.Database/Extensions/ServiceCollectionExtension.cs
+++ PP0.EntityFrameworkCore.Database/Extensions/ServiceCollectionExtension.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using PP0.EntityFrameworkCore.Database.Context;
@@ -16,6 +17,8 @@ namespace PP0.EntityFrameworkCore.Database.Extensions
         {
             services.AddDbContext<PP0DatabaseContext>(options => options.UseSqlServer(
                 configuration.GetConnectionString("PP0localdb")));
+            services.AddDefaultIdentity<IdentityUser>()
+                .AddEntityFrameworkStores<PP0DatabaseContext>();
         }
     }
 }
